8b8a73f481d606b06e107a048a480bf675aa30d9
dcr7978933k <b219021@iiit-bh.ac.in>
2021-09-20 06:40:58 -0700

    updated

--- Day8.js
+++ Day8.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const bodyParser = require("body-parser");
 const app = express();
 
 const chckadmin=(req,res,next) => {
@@ -20,6 +20,14 @@ app.get("/" ,chckadmin , (req,res) => {
     res.json(req.query)
 } );
 
+// app.use(bodyParser.urlencoded({ extented:true}));
+// app.use(bodyParser.json());
+//used for post requests
+app.use(express.urlencoded());
+app.use(express.json({ extented : true}));
 
+app.post("/", (req,res) => {
+	res.json( req.body);
+} )
 
 app.listen(5000);
